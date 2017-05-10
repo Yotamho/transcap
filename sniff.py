fd82c43dc4a542bede55f42f26c7db9e5610a375
yotamh <yotam.hochman@cyberbit.net>
2017-05-10 19:17:30 +0300

    implement sniff.py module

---
+++ sniff.py
@@ -0,0 +1,24 @@
+from subprocess import call
+from os.path import exists
+from os import makedirs
+
+def sniff(config):
+
+    interface = config['GENERAL']['interface']
+    pcap_kb_filesize = config['GENERAL']['pcap_kb_filesize']
+    pcaps_folder = config["GENERAL"]["pcaps_folder"]
+    pcaps_file_name = config["GENERAL"]["pcaps_file_name"]
+
+    if not exists(pcaps_folder):
+        makedirs(pcaps_folder)
+
+    tshark_call = "tshark -i {} -b filesize:{} -w {}\\{} -B 20".format(
+        interface, pcap_kb_filesize, pcaps_folder, pcaps_file_name, bpf_filter)
+    try:
+        print("Sniffing Started")
+        call(['cmd', '/c', tshark_call])
+    except KeyboardInterrupt:
+        print("Sniffing Stopped")
+
+if __name__ == "__main__":
+    sniff()
--- transcap.py
+++ transcap.py
@@ -1,12 +1,14 @@
 from multiprocessing import Process
 from configparser import ConfigParser
+from sniff import sniff
 
-config = ConfigParser('config.ini')
+config = ConfigParser()
+config.read('real_config.ini')
 total_processes = config['PARALLELITY']['total_processes']
 
 if __name__ == '__main__':
-    sniffer = Process()
+    sniffer = Process(target=sniff, args=[config])
     sniffer.start()
-    for process_number in range(1, total_processes + 1):
-        spooler = Process()
-        spooler.start()
+    # for process_number in range(1, total_processes + 1):
+    #     spooler = Process()
+    #     spooler.start()
