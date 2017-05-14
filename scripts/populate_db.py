06f5df70be55c2e6d03192b257915a89031f46a3
yotamh <yotam.hochman@cyberbit.net>
2017-05-14 19:26:31 +0300

    proper prepared statement

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -26,7 +26,9 @@ def insert_all(json_path):
         table_name = json_path.name[json_path.name.find('_')]
         for line in json_file:
             json_entry = loads(line)
-            session.execute(prepared_statements[table_name], [json_entry['timestamp'], json_entry['num'], line])
+            binded_statement = prepared_statements[table_name].bind([json_entry['timestamp'], json_entry['num'], line])
+            session.execute(binded_statement)
+            print(line)
         remove(str(json_path))
 
 if __name__ == "__main__":
